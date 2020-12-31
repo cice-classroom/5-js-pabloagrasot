745ead4d4e59839e66a3aafec95e543a85362413
Pablo <p.m.agrasot@hotmail.com>
2020-12-31 17:49:29 +0100

    version 2

--- src/board-app.ts
+++ src/board-app.ts
@@ -9,117 +9,63 @@ import {
 
 @customElement('board-app')
 export class BoardApp extends LitElement {
+  @property({ type: Array })
+  board = ['', '', '', '', '', '', '', '', ''];
   @property({ type: String })
-  player = '';
+  cell: 'X' | 'O' | undefined;
   @property({ type: Number })
   turn = 0;
-  @property({ type: String })
-  turnBoard = '';
+  @property({ type: Boolean })
+  clicked = false;
 
   static get styles() {
     return css`
-      .board {
-        display: grid;
-        max-width: 500px;
-        grid-template-columns: repeat(3, 150px);
-        grid-template-rows: repeat(3, 150px);
-        grid-gap: 10px;
+      .table {
+        background-color: var(--primary-color);
+        border-radius: 16px;
+        width: 500px;
+        height: 500px;
         margin: 0 auto;
+        border: 16px solid var(--front-color);
+        box-shadow: var(--shadow);
+      }
+
+      .board {
+        background-color: #999999;
+        width: 100%;
+        height: 100%;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(3, 1fr);
+        grid-gap: 6px;
       }
 
       .cell {
-        color: var(--bk-color);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 50px;
         background-color: var(--primary-color);
-        box-shadow: var(--shadow);
-        cursor: pointer;
-      }
-
-      .cell.X::before,
-      .cell.X::after,
-      .cell.O::before {
         background-color: var(--bk-color);
-      }
-
-      .board.X .cell:not(.X):not(.O):hover::before,
-      .board.X .cell:not(.X):not(.O):hover::after,
-      .board.O .cell:not(.X):not(.O):hover::before {
-        background-color: rgb(211, 211, 211, 1);
-      }
-
-      .cell.X::before,
-      .cell.X::after,
-      .board.X .cell:not(.X):not(.O):hover::before,
-      .board.X .cell:not(.X):not(.O):hover::after {
-        content: '';
-        position: absolute;
-        width: 120px;
-        height: 20px;
-      }
-
-      .cell.X::before,
-      .board.X .cell:not(.X):not(.O):hover::before {
-        transform: rotate(45deg);
-      }
-
-      .cell.X::after,
-      .board.X .cell:not(.X):not(.O):hover::after {
-        transform: rotate(-45deg);
-      }
-
-      .cell.O::before,
-      .cell.O::after,
-      .board.O .cell:not(.X):not(.O):hover::before,
-      .board.O .cell:not(.X):not(.O):hover::after {
-        content: '';
-        position: absolute;
-        border-radius: 50%;
-      }
-
-      .cell.O::before,
-      .board.O .cell:not(.X):not(.O):hover::before {
-        width: 120px;
-        height: 120px;
-      }
-
-      .cell.O::after,
-      .board.O .cell:not(.X):not(.O):hover::after {
-        width: 90px;
-        height: 90px;
-        background-color: var(--primary-color);
       }
     `;
   }
 
-  @eventOptions({ once: true, passive: true, capture: true })
-  private onCellClicked() {
-    if (this.turn % 2 == 0) {
-      this.player = 'X';
-      this.turn++;
-      this.turnBoard = 'O';
-    } else {
-      this.player = 'O';
-      this.turn++;
-      this.turnBoard = 'X';
-    }
-
-    console.log('prueba');
+  render() {
+    return html` <section class="table">
+      <div class="board">
+        ${this.board.map(
+          () =>
+            html`<div id="${this.id} @click="${this._onCellClicked}"" class="cell">${this.cell}</div>`,
+        )}
+      </div>
+    </section>`;
   }
 
-  render() {
-    return html`<section class="board ${this.turnBoard}">
-      <div @click="${this.onCellClicked}" class="cell ${this.player}"></div>
-      <div @click="${this.onCellClicked}" class="cell ${this.player}"></div>
-      <div @click="${this.onCellClicked}" class="cell ${this.player}"></div>
-      <div @click="${this.onCellClicked}" class="cell ${this.player}"></div>
-      <div @click="${this.onCellClicked}" class="cell ${this.player}"></div>
-      <div @click="${this.onCellClicked}" class="cell ${this.player}"></div>
-      <div @click="${this.onCellClicked}" class="cell ${this.player}"></div>
-      <div @click="${this.onCellClicked}" class="cell ${this.player}"></div>
-      <div @click="${this.onCellClicked}" class="cell ${this.player}"></div>
-    </section>`;
+  @eventOptions({ capture: true, once: true })
+  private _onCellClicked() {
+    if (this.turn % 2 == 0) {
+      this.cell = 'X';
+      this.turn++;
+    } else {
+      this.cell = 'O';
+      this.turn++;
+    }
   }
 }
